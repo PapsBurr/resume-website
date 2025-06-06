a89b9d6ed4213711d8a88892a5a44650688fcd63
PapsBurr <npons3932@gmail.com>
2025-06-05 21:06:11 -0600

    changes from react to nextjs

---
+++ resume-website/src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="flex min-h-screen flex-col justify-between p-24 bg-gray-600">Nathan's Resume</h1>
+  );
+}
