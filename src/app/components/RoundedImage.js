5cb951c490dd94603a445e99ec32add7f44a2c23
PapsBurr <npons3932@gmail.com>
2025-07-31 22:56:11 -0600

    attempts fix on RoundedImage component adding undefined into path

--- src/app/components/RoundedImage.js
+++ src/app/components/RoundedImage.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from "next/image";
 
 const RoundedImage = ({ src, alt, width, height }) => {
-  const srcURL = `${process.env.NEXT_PUBLIC_BASE_URL}${src}`;
+  const srcURL = process.env.NEXT_PUBLIC_BASE_URL + {src};
   return (
     <Image 
       className="rounded-[20px] shadow-md/40"
