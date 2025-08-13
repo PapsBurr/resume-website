f886c426011cf5d3013750d53a589ea216e84a7f
PapsBurr <npons3932@gmail.com>
2025-08-13 10:08:07 -0600

    removes baseurl logic from RoundedImage component

--- src/app/components/RoundedImage.js
+++ src/app/components/RoundedImage.js
@@ -3,12 +3,10 @@ import React from 'react'
 import Image from "next/image";
 
 const RoundedImage = ({ src, alt, width, height }) => {
-  const baseURL = process.env.NODE_ENV === "production" ? "/resume-website" : "";
-  const srcURL = `${baseURL}${src}`;
   return (
     <Image 
       className="rounded-[20px] shadow-md/40"
-      src = {srcURL}
+      src = {src}
       alt = {alt}
       width = {width}
       height = {height}
